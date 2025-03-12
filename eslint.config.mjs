cd8a08ffac77b390e02553c0fa9a40d568e16ee9
KrzysztofSzpakiewicz <krzysztofszpakiewicz1@gmail.com>
2025-03-12 20:14:49 +0100

    eslint, prettier, husky done, eslint conf my need improvement

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,7 +8,7 @@ const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
 	baseDirectory: __dirname,
-	baseDirectory: import.meta.dirname,
+	//baseDirectory: import.meta.dirname,
 	recommendedConfig: js.configs.recommended,
 });
 
@@ -41,67 +41,64 @@ const eslintConfig = [
 		},
 		plugins: ['prettier', '@typescript-eslint', 'react', 'react-hooks'],
 		rules: {
-			semi: ['error', 'always'], // Wymuszenie średników
-			quotes: ['error', 'single'], // Wymuszenie pojedynczych cudzysłowów
-			'prefer-arrow-callback': ['error'], // Wymuszenie użycia funkcji strzałkowych
-			'prefer-template': ['error'], // Wymuszenie używania template literals
-			'no-var': ['error'], // Wymuszenie używania let/const zamiast var
-			'no-unused-vars': ['off'], // Wymuszenie usunięcia nieużywanych zmiennych
-			'no-undef': ['error'], // Ostrzeżenie o niezdefiniowanych zmiennych
-			'no-constant-condition': ['error'], // Ostrzeżenie o warunkach z wartością stałą
-			'prefer-const': ['error'], // Ostrzeżenie o zmiennych, które powinny być const
-			'no-else-return': ['error'], // Ostrzeżenie o niepotrzebnym `else` po `return`
-			'no-useless-return': ['error'], // Ostrzeżenie o niepotrzebnym `return`
-
-			// Reguły TypeScript
-			'@typescript-eslint/no-unused-vars': ['error'], // Wymuszenie usunięcia nieużywanych zmiennych w TypeScript
-			'@typescript-eslint/explicit-function-return-type': ['error'], // Wymuszenie jawnego typowania zwracanych wartości funkcji
-			'@typescript-eslint/explicit-module-boundary-types': ['error'], // Wymuszenie jawnego typowania zwracanych wartości w eksportowanych funkcjach
-			'@typescript-eslint/no-inferrable-types': ['off'], // Wyłączenie możliwości pomijania typów, gdy są oczywiste (np. let x: number = 5)
+			semi: ['error', 'always'],
+			quotes: ['error', 'single'],
+			'prefer-arrow-callback': ['error'],
+			'prefer-template': ['error'],
+			'no-var': ['error'],
+			'no-unused-vars': ['off'],
+			'no-undef': ['error'],
+			'no-constant-condition': ['error'],
+			'prefer-const': ['error'],
+			'no-else-return': ['error'],
+			'no-useless-return': ['error'],
+			'@typescript-eslint/no-unused-vars': ['error'],
+			'@typescript-eslint/explicit-function-return-type': ['error'],
+			'@typescript-eslint/explicit-module-boundary-types': ['error'],
+			'@typescript-eslint/no-inferrable-types': ['off'],
 			'@typescript-eslint/typedef': [
 				'error',
 				{
-					variableDeclaration: true, // Wymuszenie typowania wszystkich zmiennych
-					parameter: true, // Wymuszenie typowania wszystkich parametrów funkcji
-					arrowParameter: true, // Wymuszenie typowania parametrów w funkcjach strzałkowych
-					propertyDeclaration: true, // Wymuszenie typowania właściwości w obiektach/klasach
-					memberVariableDeclaration: true, // Wymuszenie typowania zmiennych w klasach
+					variableDeclaration: true,
+					parameter: true,
+					arrowParameter: true,
+					propertyDeclaration: true,
+					memberVariableDeclaration: true,
 				},
 			],
-			'@typescript-eslint/no-explicit-any': ['error'], // Zakaz używania typu `any`
+			'@typescript-eslint/no-explicit-any': ['error'],
 			'@typescript-eslint/consistent-type-assertions': [
 				'error',
 				{
-					assertionStyle: 'as', // Wymuszenie użycia `as` zamiast `<>` do rzutowania typów
-					objectLiteralTypeAssertions: 'never', // Zakaz rzutowania literałów obiektowych
+					assertionStyle: 'as',
+					objectLiteralTypeAssertions: 'never',
 				},
 			],
 			'@typescript-eslint/no-use-before-define': [
 				'error',
 				{
-					functions: false, // Pozwala na używanie funkcji przed ich zdefiniowaniem
-					classes: true, // Zakazuje używania klas przed ich zdefiniowaniem
-					variables: true, // Zakazuje używania zmiennych przed ich zdefiniowaniem
+					functions: false,
+					classes: true,
+					variables: true,
 				},
 			],
-
 			'prettier/prettier': [
 				'error',
 				{
-					useTabs: true, // Wymusza używanie tabulatorów
-					tabWidth: 4, // Szerokość tabulatorów na 4
-					singleQuote: true, // Pojedyncze cudzysłowy
-					semi: true, // Wymuszenie średników
-					arrowParens: 'always', // Nawiasy w funkcjach strzałkowych
-					trailingComma: 'es5', // Przecinki na końcu obiektów/ tablic
-					endOfLine: 'auto', // Ustawienie końca linii
+					useTabs: true,
+					tabWidth: 4,
+					singleQuote: true,
+					semi: true,
+					arrowParens: 'always',
+					trailingComma: 'es5',
+					endOfLine: 'auto',
 				},
 			],
 			indent: [
 				'error',
-				'tab', // Używanie tabulatorów
+				'tab',
 				{
-					SwitchCase: 1, // Wcięcie dla case w switch na 1 tabulator
+					SwitchCase: 1,
 				},
 			],
 		},
